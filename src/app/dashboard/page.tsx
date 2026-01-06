1fd0b7abfa88b274fa7fecfc6414037e1360496f
yousef <yousef.ahmed7@gmail.com>
2026-01-06 14:45:35 +0200

    dashbord page added

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,12 +1,19 @@
-import Navbar from '@/components/Navbar'
-import { redirect } from 'next/dist/server/api-utils'
-import React from 'react'
+import ActivityOverview from "@/components/dashbord/ActivityOverview";
+import MainActions from "@/components/dashbord/MainActions";
+import WelcomeSection from "@/components/dashbord/WelcomeSection";
+import Navbar from "@/components/Navbar";
 
-export default function page() {
+function DashboardPage() {
   return (
-    <div>
-      dashbord page
+    <>
       <Navbar />
-    </div>
-  )
+
+      <div className="max-w-7xl mx-auto px-6 py-8 pt-24">
+        <WelcomeSection />
+        <MainActions />
+        <ActivityOverview />
+      </div>
+    </>
+  );
 }
+export default DashboardPage;
