be1686b2aec646fcf3a96e13612caf33f7ee9455
yousef <yousef.ahmed7@gmail.com>
2026-01-08 08:54:39 +0200

    small fix

--- prisma.config.ts
+++ prisma.config.ts
@@ -4,12 +4,7 @@ import { config } from "dotenv";
 config();
 
 export default defineConfig({
-  schema: "prisma/schema.prisma",
   migrations: {
     path: "prisma/migrations",
-  },
-  engine: "classic",
-  datasource: {
-    url: process.env.DATABASE_URL,
   },
 });
